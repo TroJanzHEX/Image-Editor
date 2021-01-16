709c7fb7b10514ba022700d1e926bb1203b9e6d9
ᎫᎪᏟᏦ <69492163+unknownkidnapper@users.noreply.github.com>
2021-01-16 17:24:24 +0530

    Add files via upload

---
+++ script.py
@@ -0,0 +1,30 @@
+class script(object):
+
+
+
+     START_MSG = """ <b>Hi {}
+
+I'm a Image Editor Bot which Supports various modes
+
+For more click help....</b>"""
+     HELP_MSG ="""<b>Hai, Follow these Steps..
+
+<code>🌀 Send me any Image to Edit..</code>
+
+<code>🌀Select the Corresponding mode that you need</code>
+
+<code>🌀 Your Edited Image will be Uploaded </code>
+
+
+© @TroJanzHEX"""
+     ABOUT_MSG =  """⭕️<b>My Name : Image Editor</b>
+
+⭕️<b>Language :</b> <code>Python3</code>
+
+⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
+
+⭕️<b>Source Code :</b> 👉 <a href='https://github.com/TroJanzHEX/Image-Editor'>Click Here</a>"""
+
+
+
+
