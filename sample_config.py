fe1f8a7b83d364d4df2fbd43fbfb2c800192fd1f
Fayas <76828314+FayasNoushad@users.noreply.github.com>
2021-03-04 22:21:10 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -5,11 +5,24 @@ import os
 
 
 class Config(object):
+    # get a token from @BotFather
     TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 
+    # The Telegram API things from my.telegram.org
     APP_ID = int(os.environ.get("APP_ID", 12345))
-
     API_HASH = os.environ.get("API_HASH", "")
 
     # Get this api from https://www.remove.bg/b/background-removal-api
-    RemoveBG_API = os.environ.get("RemoveBG_API", "")
+    REMOVEBG_API = os.environ.get("REMOVEBG_API", "")
+
+    # Array to store users who are authorized to use the bot
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+
+    # Update channel for Force Subscribe
+    UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "")
+
+    # dict to hold the ReQuest queue
+    ADL_BOT_RQ = {}
+
+    # set timeout for subprocess
+    PROCESS_MAX_TIMEOUT = int(os.environ.get("TIME_LIMIT", ""))
