9496a651291b10e9e53c7a585f50e166a9bfcd33
Fayas <76828314+FayasNoushad@users.noreply.github.com>
2021-03-05 17:56:40 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -6,38 +6,29 @@ from script import script  # pylint:disable=import-error
 
 @Client.on_message(filters.command(["start"]) & filters.private)
 async def start(bot, update):
-    try:
-        await client.send_message(
-            text=script.START_MSG.format(message.from_user.mention),
-            disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('🔰 HELP 🔰', callback_data='help'), InlineKeyboardButton('🔰 ABOUT 🔰', callback_data='about')]]),
-            reply_to_message_id=update.message_id,
-        )
-    except Exception:
-        pass
+    await client.send_message(
+        text=script.START_MSG.format(message.from_user.mention),
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('🔰 HELP 🔰', callback_data='help'), InlineKeyboardButton('🔰 ABOUT 🔰', callback_data='about')]]),
+        reply_to_message_id=update.message_id,
+    )
 
 
 @Client.on_message(filters.command(["help"]) & filters.private)
 async def help(bot, update):
-    try:
-        await bot.send_message(
-            text=script.HELP_MSG,
-            disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('🔰 HOME 🔰', callback_data='home'), InlineKeyboardButton('🔰 ABOUT 🔰', callback_data='about')]]),
-            reply_to_message_id=update.message_id,
-        )
-    except Exception:
-        pass
+    await bot.send_message(
+        text=script.HELP_MSG,
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('🔰 HOME 🔰', callback_data='home'), InlineKeyboardButton('🔰 ABOUT 🔰', callback_data='about')]]),
+        reply_to_message_id=update.message_id,
+    )
 
 
 @Client.on_message(filters.command(["about"]) & filters.private)
 async def about(bot, update):
-    try:
-        await bot.send_message(
-            text=script.ABOUT_MSG,
-            disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('🔰 HELP 🔰', callback_data='help'), InlineKeyboardButton('🔰 HOME 🔰', callback_data='home')]]),
-            reply_to_message_id=update.message_id,
-        )
-    except Exception:
-        pass
+    await bot.send_message(
+        text=script.ABOUT_MSG,
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('🔰 HELP 🔰', callback_data='help'), InlineKeyboardButton('🔰 HOME 🔰', callback_data='home')]]),
+        reply_to_message_id=update.message_id,
+    )
