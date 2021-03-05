7c7496525790ca087f68f6e351a492b3555e9ae4
Fayas <76828314+FayasNoushad@users.noreply.github.com>
2021-03-05 19:04:31 +0530

    Create info.py

---
+++ info.py
@@ -0,0 +1,8 @@
+class Info(object):
+    # bot info 
+    BOT_NAME = "Image Editor"
+    BOT_USERNAME = "FnImageEditBot"
+    
+    # server info
+    SERVER_DOMAIN = "heroku.com"
+    SERVER = "Heroku"
