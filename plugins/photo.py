8d461afa6475521768e4c6d4a87a41c453df332c
Fayas <76828314+FayasNoushad@users.noreply.github.com>
2021-03-05 17:00:00 +0530

    Update photo.py

--- plugins/photo.py
+++ plugins/photo.py
@@ -5,6 +5,11 @@ import math
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message 
 from pyrogram.errors import UserNotParticipant, UserBannedInChannel
+if bool(os.environ.get("WEBHOOK", False)):
+    from sample_config import Config
+else:
+    from config import Config
+
 
 @Client.on_message(filters.photo & filters.private)
 async def photo(Client, Message):
