01fe05b22290542c55f31abf5b989e69f70774ae
Fayas <76828314+FayasNoushad@users.noreply.github.com>
2021-03-10 19:51:29 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -4,20 +4,6 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from buttons import Button 
 from script import script  # pylint:disable=import-error
 
-@Client.on_callback_query()
-async def button(bot, update):
-
-    if update.data == "home":
-        await update.message.delete()
-        await start(bot, update.message)
-
-    elif update.data == "help":
-        await update.message.delete()
-        await help(bot, update.message)
-
-    elif update.data == "close":
-        await update.message.delete()
-
 @Client.on_message(filters.command(["start"]))
 async def start(bot, update):
     await bot.send_message(chat_id=update.chat.id, text=script.START_MSG.format(update.from_user.mention), parse_mode="html", disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup(Button.START_BUTTONS), reply_to_message_id=update.message_id)
