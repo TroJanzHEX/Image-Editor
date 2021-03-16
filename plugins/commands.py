b4ee2a7d3d88a30196ee62c599d5ac99c4f435bf
Fayas <76828314+FayasNoushad@users.noreply.github.com>
2021-03-16 18:20:11 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -7,7 +7,3 @@ from script import script  # pylint:disable=import-error
 @Client.on_message(filters.command(["start"]) & filters.private)
 async def start(bot, update):
     await bot.send_message(chat_id=update.chat.id, text=script.START_MSG.format(update.from_user.mention), parse_mode="html", disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup(Button.START_BUTTONS), reply_to_message_id=update.message_id)
-
-@Client.on_message(filters.command(["help"]) & filters.private)
-async def help(bot, update):
-    await bot.send_message(chat_id=update.chat.id, text=script.HELP_MSG, parse_mode="html", disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup(Button.HELP_BUTTONS), reply_to_message_id=update.message_id)
