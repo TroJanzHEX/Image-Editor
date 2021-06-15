fc464b169e2b81d8b29b88e9e2f2b3b59555b524
Disneyteam <85325455+Disneyteam76@users.noreply.github.com>
2021-06-15 20:52:18 +0530

    Update

--- plugins/commands.py
+++ plugins/commands.py
@@ -18,8 +18,8 @@ async def start(client: Client, message: Message):
                     ],
                     [
                         InlineKeyboardButton(
-                            "SOURCE CODE",
-                            url="https://github.com/TroJanzHEX/Image-Editor",
+                            "Dev",
+                            url="https://t.me/doreamonfans",
                         )
                     ],
                 ]
@@ -44,8 +44,8 @@ async def help(client, message):
                     ],
                     [
                         InlineKeyboardButton(
-                            "SOURCE CODE",
-                            url="https://github.com/TroJanzHEX/Image-Editor",
+                            "Dev",
+                            url="https://t.me/doreamonfans2",
                         )
                     ],
                 ]
@@ -70,8 +70,8 @@ async def about(client, message):
                     ],
                     [
                         InlineKeyboardButton(
-                            "SOURCE CODE",
-                            url="https://github.com/TroJanzHEX/Image-Editor",
+                            "Dev",
+                            url="https://t.me/doreamonfans3",
                         )
                     ],
                 ]
