8a5bc25fbe3e13d9baec7523253c26f5ae676dca
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-06-28 17:56:29 +0530

    Update photo.py

--- plugins/photo.py
+++ plugins/photo.py
@@ -2,6 +2,38 @@
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram import Client, filters
 
+BUTTONS = InlineKeyboardMarkup(
+      [
+          [
+              InlineKeyboardButton(text="BRIGHT", callback_data="bright"),
+              InlineKeyboardButton(text="MIXED", callback_data="mix"),
+              InlineKeyboardButton(text="B&W", callback_data="b|w"),
+          ],
+          [
+              InlineKeyboardButton(text="CIRCLE", callback_data="circle"),
+              InlineKeyboardButton(text="BLUR", callback_data="blur"),
+              InlineKeyboardButton(text="BORDER", callback_data="border"),
+          ],
+          [
+              InlineKeyboardButton(text="STICKER", callback_data="stick"),
+              InlineKeyboardButton(text="ROTATE", callback_data="rotate"),
+              InlineKeyboardButton(text="CONTRAST", callback_data="contrast"),
+          ],
+          [
+              InlineKeyboardButton(text="SEPIA", callback_data="sepia"),
+              InlineKeyboardButton(text="PENCIL", callback_data="pencil"),
+              InlineKeyboardButton(text="CARTOON", callback_data="cartoon"),
+          ],
+          [
+              InlineKeyboardButton(text="INVERT", callback_data="inverted"),
+              InlineKeyboardButton(text="GLITCH", callback_data="glitch"),
+              InlineKeyboardButton(text="REMOVE BG", callback_data="removebg"),
+          ],
+          [
+              InlineKeyboardButton(text="CLOSE", callback_data="close_e"),
+          ],
+      ]
+)
 
 @Client.on_message(filters.photo & filters.private)
 async def photo(client: Client, message: Message):
